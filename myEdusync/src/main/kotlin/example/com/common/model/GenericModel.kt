f4b29f59d5778aab2e48e67cefe8103a4dad7b95
Anit Kumar <anitkumar216@outlook.com>
2024-08-04 22:10:12 +0530

    Delete myEdusync directory

--- myEdusync/src/main/kotlin/example/com/common/model/GenericModel.kt
+++
@@ -1,3 +0,0 @@
-package example.com.common.model
-
-data class GenericResponse<out T>(val isSuccess: Boolean, val data: T)
--- myEdusync/src/main/kotlin/example/com/common/repository/DatabaseFactory.kt
+++
@@ -1,9 +0,0 @@
-package example.com.common.repository
-
-import org.litote.kmongo.coroutine.coroutine
-import org.litote.kmongo.reactivestreams.KMongo
-
-class DatabaseFactory {
-    private val client = KMongo.createClient().coroutine
-    val database = client.getDatabase("MyEduSyncDB")
-}
